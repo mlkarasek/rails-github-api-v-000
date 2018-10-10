3ad4ee5b3bf6f06899c943c0742444776cc2f732
Monica  Karasek <mlkarasek@gmail.com>
2018-10-10 21:01:32 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,14 +1,14 @@
 class RepositoriesController < ApplicationController
 
   def index
-    user = Faraday.get 'https://api.github.com/user' do |req|
+      user = Faraday.get 'https://api.github.com/user' do |req|
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
     end
 
     @user = JSON.parse(user.body)
 
-    repos = Faraday.get 'https://api.github.com/user/repos' do |req|
+      repos = Faraday.get 'https://api.github.com/user/repos' do |req|
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
     end
